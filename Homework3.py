6e2a8fa5d7bedf20d5f6c6444c9e580b38f3e3ee
Shohruh <shoh0554770876@gmail.com>
2024-06-20 18:41:31 +0300

    Add files via upload

---
+++ Homework3.py
@@ -0,0 +1,19 @@
+class Building:
+    def __init__(self, number_of_floors, building_Type):
+        self.number_of_floors = number_of_floors
+        self.building_Type = building_Type
+
+
+    def __eq__(self, other):
+        if self.number_of_floors == other.number_of_floors and self.building_Type == other.building_Type:
+            return True
+        else:
+            return False
+
+
+buiding1 = Building(4, 'Этажи ')
+buiding2 = Building(4, 'Квартиры')
+print(buiding1 == buiding2)
+buiding1 = Building(4, 'Этажи')
+buiding2 = Building(4, 'Квартиры')
+print(buiding1 == buiding2)
